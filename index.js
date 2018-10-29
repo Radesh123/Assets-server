0285cdb66d3b903abfef06abcc0f8271961074d8
Radesh123 <anagdenderraj@gmail.com>
2018-10-29 11:59:05 +0530

    cors configured

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 var app = express();
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
+app.use(allowCrossDomain);
 
 app.use(express.static(path.join(__dirname, 'public')))
+
 
 // Restfull API to get the my questions list
 app.get('/accountLookup', function (req, res) {
